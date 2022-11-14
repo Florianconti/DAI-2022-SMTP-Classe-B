b513ebd91d1c7d6bd0b1e55c5e81c876107da875
Florian <florian.conti@heig-vd.ch>
2022-11-14 16:10:32 +0100

    Group, mail et person done

--- src/main/java/SMTP/mail/Group.java
+++ src/main/java/SMTP/mail/Group.java
@@ -1,4 +1,18 @@
 package SMTP.mail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+
+    private final List<Person> persons = new ArrayList<>();
+
+    public void addPersons(Person person) {
+        persons.add(person);
+    }
+
+    public List<Person> getPersons() {
+        return new ArrayList<>(persons);
+    }
+
 }
--- src/main/java/SMTP/mail/Person.java
+++ src/main/java/SMTP/mail/Person.java
@@ -1,4 +1,27 @@
 package SMTP.mail;
 
 public class Person {
+
+    private final String firstName;
+    private final String lastName;
+    private final String mail;
+
+    public Person(String firstName, String lastName, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mail = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
 }
